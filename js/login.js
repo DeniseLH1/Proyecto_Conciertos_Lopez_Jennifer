16c5f8d296048db13ace0995d77dcc128cc8af04
DeniseLH1 <jdlhprincesa@gmail.com>
2026-03-20 13:01:27 -0600

    Feat: nueva funcion en json

--- js/login.js
+++ js/login.js
@@ -1,13 +1,14 @@
-document.addEventListener['submit',(event)=>{
+console.log("Bienvenido a Tickyiyo")
+document.querySelector('.login button').addEventListener('click',(event)=>{
     event.preventDefault()
-    const email =document.querySelector['input[type="email"]'].value
-    const password=document.querySelector['input[type="password"]'].value
+    const email =document.querySelector['input [type="email"]']
+    const password=document.querySelector['input [type="password"]']
     console.log("Datos recibidos: ",email,password)
-    if(email==='admin@gmail.com'&& password==='123456'){
+    if(email=='admin@gmail.com' && password=='123456'){
     console.log("Datos correctos")
-    window.location.href="Snooptick.html"
+    window.location="../dashboard.html"
     }
     else{
-    console.log("DATOS INCORRECTOS")
+        console.log("DATOS INCORRECTOS")
     }
-}]
+})
