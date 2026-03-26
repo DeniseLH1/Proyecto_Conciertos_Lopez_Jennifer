fd0ee71390f626f4383f7ab69f15876e6877d42d
DeniseLH1 <jdlhprincesa@gmail.com>
2026-03-26 08:09:18 -0600

    Style: nuevo estilo en login

--- js/login.js
+++ js/login.js
@@ -1,20 +1,29 @@
-console.log("Bienvenido a Tickyiyo");
-const formulario = document.querySelector('.login-form');
-if (formulario) {
-    formulario.addEventListener('submit', (event) => {
-        event.preventDefault(); 
-        const email = document.getElementById('email').value;
-        const password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', () => {
+    const formulario = document.querySelector('.login-form');
+    if (formulario) {
+        formulario.addEventListener('submit', (event) => {
+            event.preventDefault();
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
 
-        console.log("Datos capturados:", email, password);
-        if (email === 'admin@gmail.com' && password === '123456') {
-            console.log("¡DATOS CORRECTOS!");
-            window.location.href="../html/dashboard.html" 
-        } else {
-            console.log("DATOS INCORRECTOS");
-            alert("El correo o la contraseña son incorrectos");
-        }
-    });
-} else {
-    console.error("No se encontró el formulario con la clase .login-form");
-}
+            if (email === 'admin@gmail.com' && password === '123456') {
+                Swal.fire({
+                    icon: 'success',
+                    title: '¡Acceso Correcto!',
+                    text: 'Bienvenida al panel de administración',
+                    showConfirmButton: false,
+                    timer: 1500
+                }).then(() => {
+                    window.location.href="../html/dashboard.html"
+                });
+            } else {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error de acceso',
+                    text: 'Correo o contraseña incorrectos',
+                    confirmButtonColor: '#0b4a0e'
+                });
+            }
+        });
+    }
+});
